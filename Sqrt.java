c56481fdfb4a4e176e3cb8c7033dbf6b0f0b42cc
sara <saraswathi.s0495@gmail.com>
2022-02-03 18:59:07 +0530

    mySqrt

---
+++ Sqrt.java
@@ -0,0 +1,13 @@
+
+public class Sqrt {
+   public static void main(String[] args){
+        int x = 2147395599;
+       System.out.println(mySqrt(x));
+       
+    }
+  
+public static int mySqrt(int x){
+  return (int)Math.sqrt(x);
+ 
+}
+}
