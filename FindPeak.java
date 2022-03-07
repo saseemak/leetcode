a77a0d1ccf8f6979d4015e4e3e29b11c43ff6bf5
sara <saraswathi.s0495@gmail.com>
2022-03-07 11:36:33 -0600

    peak

---
+++ FindPeak.java
@@ -0,0 +1,26 @@
+public class FindPeak{
+    public static void main(String[] args){
+       int[] nums = new int[]{1,2};
+       System.out.println(findPeakElement(nums));   
+     }
+     public static int findPeakElement(int[] nums){
+         int n = nums.length;
+             int low = 0;
+             int high = n - 1;
+             while(low <= high){
+                 int mid = low + (high - low)/ 2;
+                 if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == n - 1 || nums[mid] > nums[mid+1] )){
+                     return mid;
+                 }
+                 else if(mid > 0 && nums[mid -1] > nums[mid]){
+                     high = mid -1;
+                 }
+                 else {
+                    low =  mid + 1;  
+                 }
+
+             }
+         
+         return 0;
+     }
+    }
